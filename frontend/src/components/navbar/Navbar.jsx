cbd6579c7407760ccec15cace952375229bb0436
Robert-Main <mainrobert04@gmail.com>
2024-05-02 22:45:44 +0300

    updates

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -20,9 +20,9 @@ function Navbar() {
                     <span>Luxe-Estate</span>
                 </a>
                 <a href="/">Home</a>
-                <a href="/">About</a>
-                <a href="/">Contact</a>
-                <a href="/">Agents</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+                <a href="/agents">Agents</a>
             </div>
             <div className="right">
                 {currentUser ? (
